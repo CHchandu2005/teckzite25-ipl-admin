1b9fedab66cb69f10cb9982904f1b177ae4eb004
Chintalapudi Chandra Sekhar <chanduchintalapudi9@gmail.com>
2025-02-18 16:45:49 +0530

    changes in admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; 
 import { AuthProvider } from './Authcontext';
-import IPLTeamCard from './components/TeamCard.jsx';
-// import Profilecard from './components/Profilecard.jsx';
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <App />
-      {/* <Profilecard /> */}
-      {/* <IPLTeamCard /> */}
     </AuthProvider>
 );
